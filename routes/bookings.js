ef285d4424be5a5a466bf9a20a1ddd1d48138136
Vishnu Rana <mvishnurana@gmail.com>
2021-03-27 21:54:54 +0800

    Updated post request to bookings route

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-// const { check, validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 
 // const user = require('../models/Users');
 // const bookings = require('../models/Booking');
@@ -8,9 +8,27 @@ const router = express.Router();
 // @route   POST api/bookings
 // @desc    Register a bookings takes a location and timing
 // @access  Public
-router.post('/', (req, res) => {
-    res.send('Register a new booking...');
-});
+router.post(
+    '/',
+    [
+        check('startTime', 'Please pick a specific START time for booking')
+            .not()
+            .isEmpty(),
+        check('endTime', 'Please pick a specific END time for booking')
+            .not()
+            .isEmpty(),
+        check('location', 'Please choose a booking location in facility')
+            .not()
+            .isEmpty(),
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        res.send('Register a new booking...');
+    }
+);
 
 // @route   GET api/bookings
 // desc    1. Get all bookings returns timing and location only
@@ -20,7 +38,7 @@ router.post('/', (req, res) => {
 //         1. Get all the bookings with information about who has booked it etc.
 // @access  Public
 router.get('/', (req, res) => {
-    res.send('Sending bookings...');
+    res.json({ bookings: [{ location: 'xyz' }, { location: 'abc' }] });
 });
 
 // @route   Update api/bookings
