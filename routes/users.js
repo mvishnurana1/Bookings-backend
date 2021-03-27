579a57e4bea9ed7a7a5ed5bdef033d47a31f82f2
Vishnu Rana <mvishnurana@gmail.com>
2021-03-27 21:55:45 +0800

    Added User model to the User controller

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/Users');
+
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
 router.post('/', (req, res) => {
-    res.send('Register User...');
+    res.send(req.body);
 });
 
 module.exports = router;
