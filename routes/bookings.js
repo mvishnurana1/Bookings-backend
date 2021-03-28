80e2fa0b27e2f91173d1b9d8a2ed637a5c824ba9
Vishnu Rana <mvishnurana@gmail.com>
2021-03-28 16:37:10 +0800

    Updated project dependency and fixed the booking GET request

--- routes/bookings.js
+++ routes/bookings.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 
-// const user = require('../models/Users');
-// const bookings = require('../models/Booking');
+const user = require('../models/Users');
+const AllBookings = require('../models/Booking');
 
 // @route   POST api/bookings
 // @desc    Register a bookings takes a location and timing
@@ -31,14 +31,25 @@ router.post(
 );
 
 // @route   GET api/bookings
-// desc    1. Get all bookings returns timing and location only
+// desc    1. Get all bookings returns timing and location only (for unauthenticated user)
 // Check middleware for user:
 //         1. Get all bookings for specific user and others.
 // Check middleware for Admin:
 //         1. Get all the bookings with information about who has booked it etc.
 // @access  Public
-router.get('/', (req, res) => {
-    res.json({ bookings: [{ location: 'xyz' }, { location: 'abc' }] });
+// The goal of endpoint is to only show bookings of future and nothing from past 
+// so we want less than to 
+router.get('/', async (req, res) => {
+    try {
+        const bookings = await AllBookings.find({
+            endTime: { $gte: new Date() },
+            startTime: { $lte: new Date()}
+        }).sort({ date: -1 });
+        res.json(bookings);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 // @route   Update api/bookings
