d03390689c4513aac902fb5514892769af07c992
Vishnu Rana <mvishnurana@gmail.com>
2021-03-30 03:45:11 +0800

    Made PUT route functional for updating content upon Authorisation and Authentication

--- routes/bookings.js
+++ routes/bookings.js
@@ -86,7 +86,49 @@ router.get('/', auth, async (req, res) => {
 // desc     Update any booking
 // Check middleware for [user to own before updating]/admin and check for collision.
 // @access  Private
-router.put('/:id', (req, res) => {
+router.put('/:id', auth, async (req, res) => {
+    const {
+        email,
+        endTime,
+        name,
+        location,
+        payment,
+        phone,
+        startTime,
+    } = req.body;
+
+    // Build new object
+    const updatedBooking = {};
+
+    if (email) updatedBooking.email = email;
+    if (endTime) updatedBooking.endTime = endTime;
+    if (location) updatedBooking.location = location;
+    if (name) updatedBooking.name = name;
+    if (phone) updatedBooking.phone = phone;
+    if (payment) updatedBooking.payment = payment;
+    if (startTime) updatedBooking.startTime = startTime;
+
+    try {
+        let booking = await Bookings.findById(req.params.id);
+
+        if (!booking) return res.status(404).json({ msg: 'Contact not found' });
+
+        // Make surec user owns the booking
+        if (booking.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'Not Authorised' });
+        }
+
+        booking = await Bookings.findByIdAndUpdate(
+            req.params.id,
+            { $set: updatedBooking },
+            { new: true }
+        );
+
+        res.json(booking);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
     res.send('Update booking...');
 });
 
