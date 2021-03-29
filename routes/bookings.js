d402c360433405132d6d9fa025b344514929845a
Vishnu Rana <mvishnurana@gmail.com>
2021-03-30 03:11:19 +0800

    Fixed the route for bookings GET and POST added authentication for prior to submitting their bookings

--- routes/bookings.js
+++ routes/bookings.js
@@ -4,30 +4,59 @@ const auth = require('../middleware/auth');
 const { check, validationResult } = require('express-validator');
 
 const user = require('../models/Users');
-const AllBookings = require('../models/Booking');
+const Bookings = require('../models/Booking');
 
 // @route   POST api/bookings
 // @desc    Register a bookings takes a location and timing
-// @access  Public
+// @access  Private
 router.post(
     '/',
     [
-        check('startTime', 'Please pick a specific START time for booking')
-            .not()
-            .isEmpty(),
-        check('endTime', 'Please pick a specific END time for booking')
-            .not()
-            .isEmpty(),
-        check('location', 'Please choose a booking location in facility')
-            .not()
-            .isEmpty(),
+        auth,
+        [
+            check('startTime', 'Please pick a specific START time for booking')
+                .not()
+                .isEmpty(),
+            check('endTime', 'Please pick a specific END time for booking')
+                .not()
+                .isEmpty(),
+            check('location', 'Please choose a booking location in facility')
+                .not()
+                .isEmpty(),
+        ],
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        res.send('Register a new booking...');
+
+        const {
+            location,
+            startTime,
+            endTime,
+            phone,
+            email,
+            payment,
+        } = req.body;
+
+        try {
+            const newBooking = new Bookings({
+                user: req.user.id,
+                location,
+                startTime,
+                endTime,
+                phone,
+                payment,
+                email,
+            });
+
+            const booking = await newBooking.save();
+            res.json(booking);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
     }
 );
 
@@ -42,7 +71,7 @@ router.post(
 // so we want less than to
 router.get('/', auth, async (req, res) => {
     try {
-        const bookings = await AllBookings.find({
+        const bookings = await Bookings.find({
             // endTime: { $gte: new Date() },
             startTime: { $lte: new Date() },
         }).sort({ date: -1 });
